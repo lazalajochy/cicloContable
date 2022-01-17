ba079d4b2663373ba4cc02d50be932d8d2c65cbf
jochy lazala <lazalajochy@gmail.com>
2022-01-17 00:14:40 -0400

    setting the db

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
 	database:{
 		host: 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',//'localhost',
-		user: 'dow1lz740cfc68jb', //'root',
-		password: 'ue717wla8qw1ux3w',//'12345678',
-		database: 'n16w6wyd8w5uesow'  //'esayline'
+		user:  'uhf0k4l8qgfijc9c', //'root',
+		password: 'wytdixm7f1lggt8f', //'12345678',
+		database: 'x4reh0xknq7baw7h' //'esayline'
 	}
 };
